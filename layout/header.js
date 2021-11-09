bded8726da9343af866b5c41aee012f32595168e
Mahdi Khavari <mkhavari813@gmail.com>
2021-11-09 16:50:42 +0330

    add menu bar

--- layout/header.js
+++ layout/header.js
@@ -2,14 +2,32 @@ const template = document.createElement("template");
 
 const styles = `
     <style>
+        div img{
+            width : 24px;
+            margin : 10px 20px 0;
+        }
         nav{
             background : #00247e;
+        }
+        .menu{
+            display : none
+        }
+        @media screen and (max-width: 960px) {
+            nav {
+                display : none
+            }
+            .menu{
+                display : block;
+            }
         }
     </style>
 `;
 
 template.innerHTML = `
     ${styles}
+    <div class="menu">
+        <img src="../assets/images/menu.svg" width="100px"/>
+    </div>
     <nav>
         <pages-co></pages-co>
     </nav>
@@ -18,9 +36,23 @@ template.innerHTML = `
 class Header extends HTMLElement {
   constructor() {
     super();
+    this.state = {
+        isShown : false
+    }
 
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.shadowRoot.querySelector('div img').addEventListener('click',(e)=>{
+        this.state.isShown = !this.state.isShown
+
+        if(this.state.isShown){
+            this.shadowRoot.querySelector('nav').style.display = 'block';
+        }else{
+            this.shadowRoot.querySelector('nav').style.display = 'none';
+        }
+    })
+
   }
 }
 
