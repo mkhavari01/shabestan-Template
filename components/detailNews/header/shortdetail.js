899b6093dc0c24b5f6b5d084e4831a9e9e22e178
Mahdi Khavari <mkhavari813@gmail.com>
2021-11-14 10:09:32 +0330

    add ui shortDetail component

--- components/detailNews/header/shortdetail.js
+++ components/detailNews/header/shortdetail.js
@@ -2,26 +2,55 @@ const shortDetail= document.createElement('template');
 
 shortDetailStyles = `
     <style>
-        div{
-            border : 1px solid #b8b8b8;
+        section{
+            display: flex;
+            flex-direction: row-reverse;
+            justify-content: flex-end;
         }
-        ul{
-            display : flex;
-            list-style : none;
-            justify-content : flex-start;
-            margin : 0;
-            padding : 0;
+        section div{
+            display: flex;
+            flex-direction: column;
+            margin : 0 30px;
+            word-break : break-word;
+            text-align : justify;
         }
-        li{
-            padding : 0 20px;
+        img{
+            width :312px;
+            height : 210px;
+        }
+        small{
             font-size : 12px;
+            padding-right : 10px;
+        }
+        h2{
+            font-size : 22px;
+            color : #0063BE;
+            padding-right : 10px;
+            margin : 0;
+        }
+        p{
+            background : #F2F2F2;
+            border : 1px solid #99CCFF;
+            text-align : justify;
+            margin: 0;
+            padding : 10px;
+            flex : 1;
         }
     </style>
 `
 
 shortDetail.innerHTML = `
     ${shortDetailStyles}
-    <h1>short detail test</h1>
+    <section>
+        <img src="http://media.shabestan.ir/Original/1399/12/27/IMG13393407.jpg" alt="newsPic"/>
+        <div>
+            <small> معاون پشتیبانی و توسعه منابع ستاد فهما </small>
+            <h2> کارآفرینی نقطه عطف درآمد پایدار برای کانون مساجد است/ ضرورت تاسیس وزارت مسجد </h2>
+            <p>
+                ریزانه گفت: تولید بر پایه کارآفرینی نقطه عطف درآمد پایدار برای کانون های فرهنگی و هنری مساجد است    
+            </p>
+        </div>
+    </section>
 `
 
 class ShortDetail extends HTMLElement{
