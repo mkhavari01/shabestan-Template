b25da914a2fd81a0bdd5deafa44fdedc0ec0c965
Mahdi Khavari <mkhavari813@gmail.com>
2021-11-15 14:20:13 +0330

    add media player and header

---
+++ components/multiMedia/header.js
@@ -0,0 +1,57 @@
+const multiMediaHeader = document.createElement('template');
+
+multiMediaHeaderStyle = `
+    <style>
+        nav {
+            background: #006db1;
+        }
+        ul {
+            padding : 0;
+            margin : 0;
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            list-style: none;
+        }
+        li {
+            padding: 5px 15px;
+        }
+        a {
+            text-decoration: none;
+            color: #fff;
+        }
+    </style>
+`;
+
+class MultiMediaHeader extends HTMLElement{
+    constructor(){
+        super();
+
+        this.attachShadow({mode : 'open'});
+        this.shadowRoot.innerHTML = `
+        ${multiMediaHeaderStyle}
+        <nav>
+            <ul>
+                <li style="background: #00247e;">
+                    <a href="#" style="color: #ddd;">
+                        ایتم دو 
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        ایتم یک 
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        ایتم سه 
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        `;
+        
+    }
+}
+
+window.customElements.define("multimedia-header-co",MultiMediaHeader);
---
+++ components/multiMedia/mediaPlayer.js
@@ -0,0 +1,48 @@
+const mediaPlayer = document.createElement('template');
+
+const mediaPlayerStyle = `
+    <style>
+        div {
+            position: relative;
+            width: 724px;
+            height: 413px;
+        }
+        #caption {
+            position: absolute;
+            bottom: 0;
+            height: 80px;
+            background: rgba(0, 0, 0, 0.75);
+            color: #fff;
+            padding: 10px 20px;
+            width: 94.5%;
+            opacity: 0;
+            transition: all .4s;
+            word-break : break-word;
+        }
+    </style>
+`;
+
+class MediaPlayer extends HTMLElement{
+    constructor(){
+        super();
+
+        this.attachShadow({mode : 'open'});
+        this.shadowRoot.innerHTML = `
+        ${mediaPlayerStyle}
+        <div>
+            <img src="http://media.shabestan.ir/Larg_ph/1400/08/23/IMG10312846.jpg" alt="news" width="724px" height="413px"/>
+            <div id="caption">
+                اولین جشنواره ملی هنرهای تجسمی 
+            </div>
+        </div>
+        `;
+        this.shadowRoot.querySelector('img').addEventListener('mouseover',()=>{
+            this.shadowRoot.querySelector('#caption').style.opacity = 1;
+        });
+        this.shadowRoot.querySelector('img').addEventListener('mouseout',()=>{
+            this.shadowRoot.querySelector('#caption').style.opacity = 0;
+        });
+    }
+}
+
+window.customElements.define('mediaplayer-co',MediaPlayer);
